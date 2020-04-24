8eaeb37cc2e544f1549fd630fd8d323d006b2281
chris <chrisnu@outlook.com>
2020-04-24 13:57:02 +0200

    First commit

---
+++ src/test/java/nl/dialog/DialogApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.dialog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
