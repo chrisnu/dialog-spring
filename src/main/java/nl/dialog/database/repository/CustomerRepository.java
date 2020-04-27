3f2fb8cce7fed5b48d3ebd626a9969ff3a359d58
chris <chrisnu@outlook.com>
2020-04-27 19:56:07 +0200

    Sample customer repository

---
+++ src/main/java/nl/dialog/database/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package nl.dialog.database.repository;
+
+import nl.dialog.model.entities.main.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
