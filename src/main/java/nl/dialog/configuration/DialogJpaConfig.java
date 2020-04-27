59a18bd7d0db64d584a89eafd5f05fd51b7ff178
chris <chrisnu@outlook.com>
2020-04-27 20:05:06 +0200

    Temporary disable flyway and use h2 db by default

---
+++ src/main/java/nl/dialog/configuration/DialogJpaConfig.java
@@ -0,0 +1,21 @@
+package nl.dialog.configuration;
+
+//@Configuration
+//@PropertySource("database.properties")
+//@EnableTransactionManagement
+public class DialogJpaConfig {
+
+//    @Autowired
+//    private Environment env;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(env.getProperty("datasource.driverClassName"));
+//        dataSource.setUrl(env.getProperty("datasource.url"));
+//        dataSource.setUsername(env.getProperty("datasource.username"));
+//        dataSource.setPassword(env.getProperty("datasource.password"));
+//
+//        return dataSource;
+//    }
+}
