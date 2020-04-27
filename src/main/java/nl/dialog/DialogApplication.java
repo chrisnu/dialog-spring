ce0a2d3e714bd2121f30465025ced9995affc824
chris <chrisnu@outlook.com>
2020-04-27 17:57:22 +0200

    Fix main entities and remove oauth dependency

--- src/main/java/nl/dialog/DialogApplication.java
+++ src/main/java/nl/dialog/DialogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DialogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DialogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DialogApplication.class, args);
+    }
 
 }
